7080e4506d18502ccb3dd05a0231b0008dfbd77a
toshitaka-hamamura <toshitaka.hamamura@outlook.com>
2022-08-10 10:50:49 +0900

    20220810_1

--- 1_log.md
+++ 1_log.md
@@ -1,4 +1,9 @@
+# Entry log
 ## 2022
+[8.10]
+did
+- added visualization
+
 [7.23]
 did
 - origanized the files
---
+++ mutiple_graphs_in_one_window.R
@@ -0,0 +1,7 @@
+# 4 figures arranged in 2 rows and 2 columns
+attach(mtcars)
+par(mfrow=c(2,2))
+plot(wt,mpg, main="Scatterplot of wt vs. mpg")
+plot(wt,disp, main="Scatterplot of wt vs disp")
+hist(wt, main="Histogram of wt")
+boxplot(wt, main="Boxplot of wt")
